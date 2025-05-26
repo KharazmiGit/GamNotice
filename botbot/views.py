5541ed6386958f35abd47552edcea3c211417197
KharazmiGit <Kharazmi.github@gmail.com>
2025-05-26 08:43:06 +0330

    Added some features: first, to save data in the database, and second, to avoid saving existing data in the database.

--- botbot/views.py
+++ botbot/views.py
@@ -4,100 +4,75 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.firefox import GeckoDriverManager
+from django.http import JsonResponse
+from letters.models import SummaryLetter
 
-wd = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
 
-try:
-    # Login
-    wd.get("http://kitgam.kharazmico.com")
-    WebDriverWait(wd, 20).until(EC.presence_of_element_located((By.ID, "username"))).send_keys("test")
-    wd.find_element(By.ID, "password").send_keys("Tt@123456")
-    wd.find_element(By.XPATH, "/html/body/form/div/div[1]/div/div/div[3]/div[3]/button").click()
+def scrape_letters(request):
+    wd = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
+    try:
+        # Login steps
+        wd.get("http://kitgam.kharazmico.com")
+        WebDriverWait(wd, 20).until(EC.presence_of_element_located((By.ID, "username"))).send_keys("test")
+        wd.find_element(By.ID, "password").send_keys("Tt@123456")
+        wd.find_element(By.XPATH, "/html/body/form/div/div[1]/div/div/div[3]/div[3]/button").click()
 
-    # Wait for main page
-    WebDriverWait(wd, 20).until(EC.url_contains("Index.do"))
-
-    # Switch to MAIN content iframe
-    WebDriverWait(wd, 20).until(
-        EC.frame_to_be_available_and_switch_to_it((By.ID, "content"))
-    )
-
-    # Click "New Delivers"
-    new_delivers = WebDriverWait(wd, 20).until(
-        EC.element_to_be_clickable((By.XPATH, "//span[contains(text(), 'New Delivers')]"))
-    )
-    wd.execute_script("arguments[0].click();", new_delivers)
-
-    # Switch to NESTED IFRAME
-    WebDriverWait(wd, 20).until(
-        EC.frame_to_be_available_and_switch_to_it(
-            (By.CSS_SELECTOR, "iframe[id^='UIComponent_'][id$='_contentFrame']")
+        WebDriverWait(wd, 20).until(EC.url_contains("Index.do"))
+        WebDriverWait(wd, 20).until(EC.frame_to_be_available_and_switch_to_it((By.ID, "content")))
+        new_delivers = WebDriverWait(wd, 20).until(
+            EC.element_to_be_clickable((By.XPATH, "//span[contains(text(), 'New Delivers')]"))
         )
-    )
+        wd.execute_script("arguments[0].click();", new_delivers)
 
-    # Get receiver names
-    receiver_name = WebDriverWait(wd, 30).until(
-        EC.visibility_of_all_elements_located(
-            (
-                By.XPATH,
-                '//div[contains(@class, "x-grid3-cell-inner") and contains(@class, "x-grid3-col-dlrFullName")]'
+        WebDriverWait(wd, 20).until(
+            EC.frame_to_be_available_and_switch_to_it(
+                (By.CSS_SELECTOR, "iframe[id^='UIComponent_'][id$='_contentFrame']")
             )
         )
-    )
 
-    receiver_list_name = [name.text for name in receiver_name]
-
-    # Get all letter IDs
-    letter_elements = WebDriverWait(wd, 30).until(
-        EC.visibility_of_all_elements_located(
-            (By.CSS_SELECTOR, "div.x-grid3-cell-inner.x-grid3-col-letId")
+        receiver_names = WebDriverWait(wd, 30).until(
+            EC.visibility_of_all_elements_located((By.XPATH, '//div[contains(@class, "x-grid3-col-dlrFullName")]'))
         )
-    )
-    letter_ids = [elem.text for elem in letter_elements]
+        receiver_list_name = [name.text for name in receiver_names]
 
-    # Get all sender names - IMPORTANT: We're still in the same iframe context
-    sender_elements = WebDriverWait(wd, 30).until(
-        EC.visibility_of_all_elements_located(
-            (By.XPATH,
-             "//div[contains(@class, 'x-grid3-cell-inner') and contains(@class, 'x-grid3-col-dlvSenderFullName')]//div[contains(@class, 'longStringRender')]")
+        letter_elements = WebDriverWait(wd, 30).until(
+            EC.visibility_of_all_elements_located((By.CSS_SELECTOR, "div.x-grid3-col-letId"))
         )
-    )
+        letter_ids = [elem.text for elem in letter_elements]
 
-    sender_names = [name.text for name in sender_elements]
-
-    # Get all time that message sent
-    time_sent_elements = WebDriverWait(wd, 30).until(
-        EC.visibility_of_all_elements_located(
-            (By.XPATH, '//div[contains(@class, "x-grid3-cell-inner") and contains(@class, "x-grid3-col-dlvDate")]')
+        sender_elements = WebDriverWait(wd, 30).until(
+            EC.visibility_of_all_elements_located(
+                (By.XPATH, "//div[contains(@class, 'x-grid3-col-dlvSenderFullName')]//div"))
         )
-    )
+        sender_names = [elem.text for elem in sender_elements]
 
-    time_sent_list = [time.text for time in time_sent_elements]
-
-    # Get all subjects
-    subject_elements = WebDriverWait(wd, 30).until(
-        EC.visibility_of_all_elements_located(
-            (By.XPATH,
-             '//div[contains(@class, "x-grid3-cell-inner") and contains(@class, "x-grid3-col-dlvComment")]//div[contains(@class, "longStringRender")]')
+        time_sent_elements = WebDriverWait(wd, 30).until(
+            EC.visibility_of_all_elements_located((By.XPATH, "//div[contains(@class, 'x-grid3-col-dlvDate')]"))
         )
-    )
+        time_sent_list = [elem.text for elem in time_sent_elements]
 
-    subject_list = [sub.text for sub in subject_elements]  # Fixed variable name
+        letters_data = []
+        for i in range(len(letter_ids)):
+            letters_data.append({
+                "letter_id": letter_ids[i],
+                "sender": sender_names[i],
+                "receiver": receiver_list_name[i],
+                "sent_time": time_sent_list[i],
+            })
 
-    letters_data = []
-    for i in range(len(letter_ids)):
-        letter = {
-            "letter_id": letter_ids[i],
-            "sender": sender_names[i],
-            "receiver": receiver_list_name[i],
-            "time_sent": time_sent_list[i],
-            "subject": subject_list[i]
-        }
-        letters_data.append(letter)
+        for item in letters_data:
+            if not SummaryLetter.objects.filter(letter_id=item['letter_id'], sent_time=item['sent_time']).exists():
+                SummaryLetter.objects.create(
+                    letter_id=item['letter_id'],
+                    sender=item['sender'],
+                    receiver=item['receiver'],
+                    sent_time=item['sent_time'],
+                )
 
-    for letter in letters_data:
-        print(letter)
+        return JsonResponse({"status": "success", "letters": letters_data})
 
+    except Exception as e:
+        return JsonResponse({"status": "error", "message": str(e)}, status=500)
 
-except Exception as e:
-    print(f"Error: {str(e)}")
+    finally:
+        wd.quit()
--- letters/models.py
+++ letters/models.py
@@ -5,9 +5,8 @@ class SummaryLetter(models.Model):
     receiver = models.CharField(max_length=200)
     letter_id = models.CharField(max_length=10)
     sent_time = models.CharField(max_length=300)
-    subject = models.CharField(max_length=500)
     create_at = models.DateTimeField(auto_now_add=True)
     sent = models.BooleanField(default=False)
 
     def __str__(self):
-        return f"{self.sender} sent a letter to {self.receiver} it's subject is {self.subject}"
+        return f"{self.sender} sent a letter to {self.receiver}"
