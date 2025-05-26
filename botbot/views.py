8d9eed17cdd6c2977a69938245ca891ca4690fe7
KharazmiGit <Kharazmi.github@gmail.com>
2025-05-26 14:17:45 +0330

    pagination problem has solved.

--- botbot/views.py
+++ botbot/views.py
@@ -6,7 +6,8 @@ from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.firefox import GeckoDriverManager
 from django.http import JsonResponse
 from letters.models import SummaryLetter
-
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.keys import Keys
 
 def scrape_letters(request):
     wd = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
@@ -30,39 +31,76 @@ def scrape_letters(request):
             )
         )
 
-        receiver_names = WebDriverWait(wd, 30).until(
-            EC.visibility_of_all_elements_located((By.XPATH, '//div[contains(@class, "x-grid3-col-dlrFullName")]'))
-        )
-        receiver_list_name = [name.text for name in receiver_names]
-
-        letter_elements = WebDriverWait(wd, 30).until(
-            EC.visibility_of_all_elements_located((By.CSS_SELECTOR, "div.x-grid3-col-letId"))
-        )
-        letter_ids = [elem.text for elem in letter_elements]
-
-        sender_elements = WebDriverWait(wd, 30).until(
-            EC.visibility_of_all_elements_located(
-                (By.XPATH, "//div[contains(@class, 'x-grid3-col-dlvSenderFullName')]//div"))
-        )
-        sender_names = [elem.text for elem in sender_elements]
-
-        time_sent_elements = WebDriverWait(wd, 30).until(
-            EC.visibility_of_all_elements_located((By.XPATH, "//div[contains(@class, 'x-grid3-col-dlvDate')]"))
-        )
-        time_sent_list = [elem.text for elem in time_sent_elements]
-
         letters_data = []
-        for i in range(len(letter_ids)):
-            letters_data.append({
-                "letter_id": letter_ids[i],
-                "sender": sender_names[i],
-                "receiver": receiver_list_name[i],
-                "sent_time": time_sent_list[i],
-            })
 
+        # Get total pages
+        total_pages = int(WebDriverWait(wd, 20).until(
+            EC.visibility_of_element_located((By.ID, "ext-gen76"))
+        ).text.split()[-1])
+
+        for current_page in range(1, total_pages + 1):
+            # Extract data from current page
+            receiver_names = WebDriverWait(wd, 30).until(
+                EC.visibility_of_all_elements_located((By.XPATH, '//div[contains(@class, "x-grid3-col-dlrFullName")]'))
+            )
+            receiver_list_name = [name.text for name in receiver_names]
+
+            letter_elements = WebDriverWait(wd, 30).until(
+                EC.visibility_of_all_elements_located((By.CSS_SELECTOR, "div.x-grid3-col-letId"))
+            )
+            letter_ids = [elem.text for elem in letter_elements]
+
+            sender_elements = WebDriverWait(wd, 30).until(
+                EC.visibility_of_all_elements_located(
+                    (By.XPATH, "//div[contains(@class, 'x-grid3-col-dlvSenderFullName')]//div"))
+            )
+            sender_names = [elem.text for elem in sender_elements]
+
+            time_sent_elements = WebDriverWait(wd, 30).until(
+                EC.visibility_of_all_elements_located((By.XPATH, "//div[contains(@class, 'x-grid3-col-dlvDate')]"))
+            )
+            time_sent_list = [elem.text for elem in time_sent_elements]
+
+            # Append data to the list
+            for i in range(len(letter_ids)):
+                letters_data.append({
+                    "letter_id": letter_ids[i],
+                    "sender": sender_names[i],
+                    "receiver": receiver_list_name[i],
+                    "sent_time": time_sent_list[i],
+                })
+
+            # Stop if we're on the last page
+            if current_page == total_pages:
+                break
+
+            # Navigate to next page
+            page_input = WebDriverWait(wd, 20).until(
+                EC.presence_of_element_located((By.CLASS_NAME, 'x-tbar-page-number'))
+            )
+
+            page_input.clear()
+            page_input.send_keys(str(current_page + 1))  # Fixed missing closing parenthesis
+            page_input.send_keys(Keys.RETURN)
+
+            # Wait for grid refresh
+            WebDriverWait(wd, 30).until(
+                EC.staleness_of(letter_elements[0])
+            )
+
+            # Re-switch to frame after navigation
+            wd.switch_to.default_content()
+            WebDriverWait(wd, 20).until(EC.frame_to_be_available_and_switch_to_it((By.ID, "content")))
+            WebDriverWait(wd, 20).until(
+                EC.frame_to_be_available_and_switch_to_it(
+                    (By.CSS_SELECTOR, "iframe[id^='UIComponent_'][id$='_contentFrame']")
+                )
+            )
+
+        # Save data to database
         for item in letters_data:
             if not SummaryLetter.objects.filter(letter_id=item['letter_id'], sent_time=item['sent_time']).exists():
-                SummaryLetter.objects.create(
+                SummaryLetter.objects.create(  # Fixed indentation and removed unnecessary backslash
                     letter_id=item['letter_id'],
                     sender=item['sender'],
                     receiver=item['receiver'],
@@ -73,6 +111,4 @@ def scrape_letters(request):
 
     except Exception as e:
         return JsonResponse({"status": "error", "message": str(e)}, status=500)
-
-    finally:
-        wd.quit()
+    find
